6c12a5d0b3ae9e170c3811b1c368ecee28c1876f
Aljonard Dela Cruz <aljonarddc@gmail.com>
2024-11-26 16:48:55 +0800

    Initial Commit

---
+++ components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div className='grid place-content-center w-screen h-screen'>
+      <a href="#" className='text-xl font-semibold p-4 border-2 border-black'> Button</a>
+    </div>
+  )
+}
+
+export default Button
