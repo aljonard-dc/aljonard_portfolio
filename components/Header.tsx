b00c82377a16ff0c76eaa9de7de03815f7bbf77d
Aljonard Dela Cruz <aljonarddc@gmail.com>
2024-12-09 20:18:55 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,15 @@ const Header = () => {
   return (
     <div>
       <img src="logo-black" alt="" />
+
+      <div> 
+        <ul>
+            <li> Home </li>
+            <li> Projects </li>
+            <li> About </li>
+            <li> Contact </li>
+        </ul>
+      </div>
     </div>
   )
 }
